9e1b1cbaf50f979358e3ea65dea7ce09cd08e3cf
Marcelo Di Jorge <marcelobdij@gmail.com>
2023-04-22 14:35:34 -0300

    feat: add the propertyController and make alterations to the .env file

--- backend-imobiliaria/controllers/propertyController.js
+++ backend-imobiliaria/controllers/propertyController.js
@@ -2,25 +2,76 @@ const Property = require('../models/Property');
 
 // Create a new property
 exports.createProperty = async (req, res) => {
-    // ... Your create property logic here
+    try {
+        const newProperty = new Property({ ...req.body, createdBy: req.user.id });
+        const property = await newProperty.save();
+        res.json(property);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
 };
 
 // Get all properties
 exports.getAllProperties = async (req, res) => {
-    // ... Your get all properties logic here
+    try {
+        const properties = await Property.find();
+        res.json(properties);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
 };
 
 // Get property by ID
 exports.getPropertyById = async (req, res) => {
-    // ... Your get property by ID logic here
+    try {
+        const property = await Property.findById(req.params.id);
+
+        if (!property) {
+            return res.status(404).json({ message: 'Property not found' });
+        }
+
+        res.json(property);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
 };
 
 // Update property
 exports.updateProperty = async (req, res) => {
-    // ... Your update property logic here
+    try {
+        const property = await Property.findById(req.params.id);
+
+        if (!property) {
+            return res.status(404).json({ message: 'Property not found' });
+        }
+
+        if (property.createdBy.toString() !== req.user.id) {
+            return res.status(403).json({ message: 'Not authorized to update this property' });
+        }
+
+        const updatedProperty = await Property.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedProperty);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
 };
 
 // Delete property
 exports.deleteProperty = async (req, res) => {
-    // ... Your delete property logic here
+    try {
+        const property = await Property.findById(req.params.id);
+
+        if (!property) {
+            return res.status(404).json({ message: 'Property not found' });
+        }
+
+        if (property.createdBy.toString() !== req.user.id) {
+            return res.status(403).json({ message: 'Not authorized to delete this property' });
+        }
+
+        await Property.findByIdAndRemove(req.params.id);
+        res.json({ message: 'Property deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
 };
